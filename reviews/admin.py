895222a33069e31445660e3f21034726c8e017eb
Yoonsik-Shin <sys25800994@gmail.com>
2022-10-21 11:41:31 +0900

    feat : reviews admin
    - reviews admin.py 작성

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Review
 
 # Register your models here.
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = '__all__'
+
+admin.site.register(Review, ReviewAdmin)
