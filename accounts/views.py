7c537ea12767083a798bf2e30eff2441eced3afa
Yoonsik-Shin <sys25800994@gmail.com>
2022-10-21 10:58:19 +0900

    feat : login function
    - signup, login form 태그에 csrf_token 추가
    - login template 추가
    - login view 추가
    - login url 추가
    - admin 계정 추가

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from accounts.forms import CustomUserCreationForm, CustomUserChangeForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
 # Create your views here.
 def signup(request):
@@ -13,3 +16,15 @@ def signup(request):
         'signup_form': signup_form,
     }
     return render(request, 'accounts/signup.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        login_form = AuthenticationForm(request, data=request.POST)
+        if login_form.is_valid():
+            auth_login(request, login_form.get_user())
+            return redirect(request.GET.get('next') or 'accounts:login')
+    login_form = AuthenticationForm()
+    context = {
+        'login_form': login_form,
+    }
+    return render(request, 'accounts/login.html', context)
