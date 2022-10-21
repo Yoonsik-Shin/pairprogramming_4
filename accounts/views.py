ef83e1a25760a492af62d74d5ff77d45f2dda19f
Yoonsik-Shin <sys25800994@gmail.com>
2022-10-21 13:33:38 +0900

    feat / refact : index page / create
    - reviews index template 구현
    - reviews index view 구현
    - reviews index.html 구현
    - reviews create 유저 정보 저장 추가
    - accounts views.py redirect값 accounts:index로 리팩토링

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from accounts.forms import CustomUserCreationForm, CustomUserChangeForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def signup(request):
@@ -10,7 +11,7 @@ def signup(request):
         signup_form = CustomUserCreationForm(request.POST)
         if signup_form.is_valid():
             signup_form.save()
-            return redirect('accounts:signup')
+            return redirect('reviews:index')
     else:
         signup_form = CustomUserCreationForm()
     context = {
@@ -23,7 +24,7 @@ def login(request):
         login_form = AuthenticationForm(request, data=request.POST)
         if login_form.is_valid():
             auth_login(request, login_form.get_user())
-            return redirect(request.GET.get('next') or 'accounts:login')
+            return redirect(request.GET.get('next') or 'reviews:index')
     else:
         login_form = AuthenticationForm()
     context = {
@@ -31,6 +32,7 @@ def login(request):
     }
     return render(request, 'accounts/login.html', context)
 
+@login_required
 def logout(request):
     auth_logout(request)
-    return redirect('accounts:login')
+    return redirect('reviews:index')
--- reviews/views.py
+++ reviews/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render
 from .models import Review
 from .forms import ReviewForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def create(request):
     if request.method == 'POST':
         review_form = ReviewForm(request.POST)
         if review_form.is_valid():
-            review_form.save()
+            review = review_form.save(commit=False)
+            review.user = request.user
+            review.save()
     else:
         review_form = ReviewForm()
     context = {
         'review_form': review_form,
     }
     return render(request, 'reviews/form.html', context)
+
+def index(request):
+    reviews = Review.objects.all()
+    context = {
+        'reviews': reviews,
+    }
+    return render(request, 'reviews/index.html', context)
